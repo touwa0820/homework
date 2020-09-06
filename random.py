463c2d8b65a3355c88289b2498d8c29cbeccbb93
“touwa0820” <touwa100@gmail.com>
2020-09-06 16:21:34 +0900

    random2

--- random.py
+++ random.py
@@ -2,4 +2,5 @@ from random import randint
 a = randint(1,10)
 b = randint(1,10)
 c = randint(1,10)
+
 print(a,b,c)
---
+++ ransu.py
@@ -0,0 +1,6 @@
+from random import randint
+a = randint(1,10)
+b = randint(1,10)
+c = randint(1,10)
+
+print("生成された乱数は",a,b,c,"最大値は",max(a,b,c),"最小値は",min(a,b,c),"でした。")
