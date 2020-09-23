6cf44545de816d5e53a4bbcf68ad6bb8b5bb50d8
“touwa0820” <touwa100@gmail.com>
2020-09-23 16:13:24 +0900

    list_tuple

---
+++ python_list.py
@@ -0,0 +1,13 @@
+r201 = "青木"
+r202 = "広田"
+r203 = "野村"
+r101 = "佐藤"
+r102 = "田中"
+r103 = "鈴木"
+
+floor1 = [r101,r102,r103]
+floor2 = [r201,r202,r203]
+
+apartment = [floor1,floor2]
+
+print(apartment)
---
+++ python_tuple_set.py
@@ -0,0 +1,11 @@
+list100 = list(range(1,100))
+
+set_x5 = set(list100[4::5])
+
+print(set_x5)
+
+set_x7 = set(list100[6::7])
+
+print(set_x7)
+
+print(set_x5&set_x7)
